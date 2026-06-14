57815e9fe3aa612b71c5e06bb33f87e6170f524c
Lynn Yserbyt <gizzylynne@gmail.com>
2026-06-14 14:46:47 +0300

    fix sanity image type import

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@sanity/client'
 import { createImageUrlBuilder } from '@sanity/image-url'
-import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
 export const client = createClient({
   projectId: 'djjar001',
@@ -11,6 +10,7 @@ export const client = createClient({
 
 const builder = createImageUrlBuilder(client)
 
-export function urlFor(source: SanityImageSource) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function urlFor(source: any) {
   return builder.image(source)
 }
